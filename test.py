5c030a8941e3a2e15fc35bb51710d8a669a8abce
Iqbal-hal <iqbal.hal@gmail.com>
2025-09-20 16:57:34 +0530

    test.py and test_input are created for tutorial

--- test.py
+++ test.py
@@ -8,13 +8,14 @@ import glob
 #print(os.path.abspath(__file__))
 
 folder_path = os.path.dirname(os.path.abspath(__file__))
-print(f"The directory is: {folder_path}")
-INPUT_DATA = os.path.join(folder_path, "input_data")
-print(f"\n'input_data' relative path: {INPUT_DATA}")
+#print(f"The directory is: {folder_path}")
+INPUT_DATA = os.path.join(folder_path, "test_input")
+#print(f"\n'test_input' relative path: {INPUT_DATA}")
 pattern = os.path.join(INPUT_DATA, "*.csv")
-print(f"\nThe pattern is: {pattern}")
+print(f"\n The pattern is: {pattern}")
 files = glob.glob(pattern)
 print(f"\nThe files are: {files}")
-for file in files:
-    print(f"\nThe file is: {file}")
+print(f"type(files): {type(files)}")
+
+
 
